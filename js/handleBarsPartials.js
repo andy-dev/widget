f4fa7877335f63d0fb1e224fd919a5182b98e7bf
andy-dev <andres.macedo@gmail.com>
2016-08-29 21:08:51 -0500

    refactor

---
+++ js/handleBarsPartials.js
@@ -0,0 +1,22 @@
+module.exports = function(){
+	var Handlebars = require('handlebars-template-loader/runtime');
+	var watchVideosPartial = require('../templates/mainMenu/_watchVideosPartial.hbs'),
+	 		programmingPartial = require('../templates/mainMenu/_programmingPartial.hbs'),
+			engagePromotePartial = require('../templates/mainMenu/_engagePromotePartial.hbs'),
+			developmentPartial  = require('../templates/mainMenu/_developmentPartial.hbs'),
+			stationManagementPartial  = require('../templates/mainMenu/_stationManagementPartial.hbs'),
+			feedsPartial = require('../templates/mainMenu/_feedsPartial.hbs'),
+			subMenuRightFirstPartial  = require('../templates/subMenu/_subMenuRightFirstPartial.hbs')
+			subMenuRightRemainingPartials = require('../templates/subMenu/_subMenuRightRemainingPartials.hbs');
+
+	Handlebars.registerPartial({
+		watchVideosPartial            :watchVideosPartial,
+		programmingPartial            :programmingPartial,
+		engagePromotePartial          :engagePromotePartial,
+		developmentPartial            :developmentPartial,
+		stationManagementPartial      :stationManagementPartial,
+		feedsPartial                  :feedsPartial,
+		subMenuRightFirstPartial      :subMenuRightFirstPartial,
+		subMenuRightRemainingPartials :subMenuRightRemainingPartials 
+	});
+}
